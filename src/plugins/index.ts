7d27be6dd2fd15ebdcb3b5e91a6cf53e018aa910
lukaasz555 <lukasz.zegar555@gmail.com>
2023-10-29 20:21:53 +0100

    added i18n

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -8,10 +8,11 @@
 import vuetify from "./vuetify";
 import pinia from "../Global/store";
 import router from "@/Global/router";
+import i18n from "./i18n";
 
 // Types
 import type { App } from "vue";
 
 export function registerPlugins(app: App) {
-  app.use(vuetify).use(router).use(pinia);
+  app.use(vuetify).use(router).use(pinia).use(i18n);
 }
