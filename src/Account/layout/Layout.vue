bcb1fdb91bb5b0ac72452dc6ee77c81d02bd4f4e
lukaasz555 <lukasz.zegar555@gmail.com>
2023-10-31 09:18:33 +0100

    quick layout update

--- src/Account/layout/Layout.vue
+++ src/Account/layout/Layout.vue
@@ -1,25 +1,21 @@
 <template>
   <div class="container">
-    <header>LinkManager</header>
+    <Header />
     <main>
       <router-view></router-view>
     </main>
   </div>
 </template>
 
+<script setup lang="ts">
+import Header from "@/Global/components/Header.vue";
+</script>
+
 <style scoped lang="scss">
 @import "../../styles/variables.scss";
 .container {
   width: 90%;
   transition: all 0.15s;
-  header {
-    font-size: 38px;
-    // font-family? font-weight?
-    color: $col-navy-base;
-    text-align: center;
-    margin-bottom: 0.25em;
-  }
-
   main {
     padding: 2em 3em 0.5em;
     border-radius: $border-radius-outer;
---
+++ src/Main/layout/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <Header />
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Header from "@/Global/components/Header.vue";
+</script>
