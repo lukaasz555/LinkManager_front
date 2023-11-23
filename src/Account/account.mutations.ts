9df09d3a1676c985288402f234415fd19bac3bd6
lukaasz555 <lukasz.zegar555@gmail.com>
2023-11-23 20:50:24 +0100

    auth mutations update

--- src/Account/account.mutations.ts
+++ src/Account/account.mutations.ts
@@ -1,18 +1,39 @@
 import { useAppStore } from "@/Global/store/app";
 import { TranslateResult } from "vue-i18n";
+import { AccountDto, accountLogin, accountRegister } from "./account.service";
+import { useAccountStore } from "./account.store";
 
-export async function accountMutation<T extends object>(
-  handler: Function,
-  dto: T,
+export async function registerMutation(
+  accountDto: AccountDto,
   successMessage: TranslateResult
 ): Promise<void> {
   const appStore = useAppStore();
   appStore.setLoading();
 
   try {
-    await handler(dto);
+    await accountRegister(accountDto);
     appStore.setSuccess(true, successMessage);
     appStore.setError(false);
+  } catch (err: any) {
+    appStore.setError(true, err.errorMessage);
+  } finally {
+    appStore.setLoading();
+  }
+}
+
+export async function loginMutation(
+  accountDto: AccountDto,
+  successMessage: TranslateResult
+): Promise<void> {
+  const accountStore = useAccountStore();
+  const appStore = useAppStore();
+  appStore.setLoading();
+
+  try {
+    const token = (await accountLogin(accountDto)) as unknown as string;
+    appStore.setSuccess(true, successMessage);
+    appStore.setError(false);
+    accountStore.setTokenAndLoggedIn(token);
   } catch (err: any) {
     appStore.setError(true, err.errorMessage);
   } finally {
--- src/Account/account.store.ts
+++ src/Account/account.store.ts
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
-import { AccountDto, accountLogin, accountRegister } from "./account.service";
-import { accountMutation } from "./account.mutations";
+import { AccountDto } from "./account.service";
+import { registerMutation, loginMutation } from "./account.mutations";
 import i18n from "@/plugins/i18n";
 
 type BaseUserData = {
@@ -23,18 +23,18 @@ export const useAccountStore = defineStore("accountStore", {
   state: initState,
   actions: {
     async login(accountDto: AccountDto): Promise<void> {
-      await accountMutation<AccountDto>(
-        accountLogin,
-        accountDto,
-        i18n.global.t("Login success")
-      );
+      await loginMutation(accountDto, i18n.global.t("Login success"));
     },
     async register(accountDto: AccountDto) {
-      await accountMutation<AccountDto>(
-        accountRegister,
+      await registerMutation(
         accountDto,
         i18n.global.t("Success ! New account created.")
       );
+    },
+
+    setTokenAndLoggedIn(token: string): void {
+      this.token = token;
+      this.isLoggedIn = true;
     },
 
     logout(): void {
