2559ce3ba8e0614915381b27ec92123b1c0417be
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-12-16 14:23:46 +0100

    router cleanup

--- src/Global/router/index.ts
+++ src/Global/router/index.ts
@@ -2,29 +2,9 @@ import { createRouter, createWebHistory } from "vue-router";
 import accountRoutes from "@/Account/account.routes";
 import mainRoutes from "@/Main/main.routes";
 import { AppRoute } from "../interfaces/AppRoute";
-import { MainRoutesNames } from "@/Main/enums/MainRoutesNames.enum";
 import { AccountRoutesNames } from "@/Account/enums/AccountRoutesNames.enum";
 import { useAccountStore } from "@/Account/account.store";
-import { isTokenValid } from "../utils/api";
-
-interface DecodedToken {
-  sub: string;
-  email: string;
-  iat: number;
-  exp: number;
-}
-
-function getTokenFromLocalStorage(): void {
-  const token = localStorage.getItem("user-token");
-  if (token) {
-    const decodedToken: DecodedToken = JSON.parse(atob(token?.split(".")[1]));
-    if (decodedToken.exp * 1000 < Date.now()) {
-      useAccountStore().setBaseUserData(decodedToken.sub, decodedToken.email);
-      useAccountStore().token = token;
-      useAccountStore().isLoggedIn = true;
-    }
-  } else console.log("no token");
-}
+import { getTokenFromLS } from "@/Account/helpers/getTokenFromLS";
 
 const routes = [...accountRoutes, ...mainRoutes];
 
@@ -33,9 +13,17 @@ const router = createRouter({
   routes,
 });
 
-function isUserAuthenthicated(): void {
-  const token = localStorage.getItem("user-token");
-}
+router.beforeEach((to, from, next) => {
+  const route = to as unknown as AppRoute;
+  if (route.meta?.isLoginRequired) {
+    getTokenFromLS();
+    const accountStore = useAccountStore();
+    if (!accountStore.isLoggedIn) {
+      router.push({ name: AccountRoutesNames.Login });
+    }
+  }
+  next();
+});
 
 router.beforeEach((to, from, next) => {
   const route = to as unknown as AppRoute;
@@ -43,13 +31,6 @@ router.beforeEach((to, from, next) => {
     document.title =
       route.meta.title.substring(0, 1).toUpperCase() +
       route.meta.title.substring(1);
-  }
-  if (route.meta?.isLoginRequired) {
-    const accountStore = useAccountStore();
-    getTokenFromLocalStorage();
-    if (!accountStore.isLoggedIn) {
-      router.push({ name: AccountRoutesNames.Login });
-    }
   }
   next();
 });
